c9b6da1c8542107843004ffe143b13a2a619d728
DGN4053 <106657019+DGN4053@users.noreply.github.com>
2022-06-28 15:52:59 +0100

    FirstSoundsInUnity
    Issues #25 #26 #27 #28 #29 & #31 Have all been placed within Wwise & Unity. Issues with #31 sound not triggering in unity this will be further investigated.

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -11,6 +11,34 @@
 
 namespace AK
 {
+    namespace EVENTS
+    {
+        static const AkUniqueID CRYSTAL1ACTIVATE = 3539507669U;
+        static const AkUniqueID CRYSTAL2ACTIVATE = 1179112722U;
+        static const AkUniqueID CRYSTAL3ACTIVATE = 1169711155U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LAND = 674522502U;
+        static const AkUniqueID PLAYHEALTHPICKUP = 23853805U;
+        static const AkUniqueID PLAYHEALTHRELEASE = 1810625332U;
+        static const AkUniqueID PLAYSPACESHIPALARM = 1792115892U;
+        static const AkUniqueID PLAYSPACESHIPIDLE = 3078778343U;
+        static const AkUniqueID PLAYWATER = 2706800728U;
+        static const AkUniqueID PLAYWIND = 1592727253U;
+        static const AkUniqueID PLAYWINDALT = 3423484422U;
+        static const AkUniqueID WEAPONSWINGNOCONTACT = 1390420672U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID GAMEWINDGAIN = 1091409744U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID LEVEL1SOUNDBANK = 1151086105U;
+    } // namespace BANKS
+
     namespace BUSSES
     {
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
